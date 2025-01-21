68ed7c93f85de183683997811bfb887c087b2fd2
Charles Walton <pythontutorials36@gmail.com>
2025-01-21 12:24:59 +0100

    Adding the View fucntion for proper action

--- core/views.py
+++ core/views.py
@@ -1,3 +1,81 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Notebook, Note, NoteCategory
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class NotebookListView(LoginRequiredMixin, ListView):
+    model = Notebook
+    template_name = 'core/notebook_list.html'
+    context_object_name = 'notebooks'
+
+    def get_queryset(self):
+        return self.model.objects.filter(owner=self.request.user)
+
+class NotebookDetailView(LoginRequiredMixin, DetailView):
+    model = Notebook
+    template_name = 'core/notebook_detail.html'
+    context_object_name = 'notebook'
+
+class NotebookCreateView(LoginRequiredMixin, CreateView):
+    model = Notebook
+    fields = ['name', 'description']
+    template_name = 'core/notebook_form.html'
+    success_url = reverse_lazy('notebook-list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class NotebookUpdateView(LoginRequiredMixin, UpdateView):
+    model = Notebook
+    fields = ['name', 'description']
+    template_name = 'core/notebook_form.html'
+    success_url = reverse_lazy('notebook-list')
+
+class NotebookDeleteView(LoginRequiredMixin, DeleteView):
+    model = Notebook
+    template_name = 'core/notebook_confirm_delete.html'
+    success_url = reverse_lazy('notebook-list')
+
+class NoteListView(LoginRequiredMixin, ListView):
+    model = Note
+    template_name = 'core/note_list.html'
+    context_object_name = 'notes'
+
+    def get_queryset(self):
+        return self.model.objects.filter(notebook__owner=self.request.user)
+
+class NoteDetailView(LoginRequiredMixin, DetailView):
+    model = Note
+    template_name = 'core/note_detail.html'
+    context_object_name = 'note'
+
+class NoteCreateView(LoginRequiredMixin, CreateView):
+    model = Note
+    fields = ['title', 'content', 'notebook', 'category', 'is_archived', 'is_pinned']
+    template_name = 'core/note_form.html'
+    success_url = reverse_lazy('note-list')
+
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+        form.fields['notebook'].queryset = Notebook.objects.filter(owner=self.request.user)
+        return form
+
+class NoteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Note
+    fields = ['title', 'content', 'notebook', 'category', 'is_archived', 'is_pinned']
+    template_name = 'core/note_form.html'
+    success_url = reverse_lazy('note-list')
+
+    def get_form(self, *args, **kwargs):
+        form = super().get_form(*args, **kwargs)
+        form.fields['notebook'].queryset = Notebook.objects.filter(owner=self.request.user)
+        return form
+
+class NoteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Note
+    template_name = 'core/note_confirm_delete.html'
+    success_url = reverse_lazy('note-list')
