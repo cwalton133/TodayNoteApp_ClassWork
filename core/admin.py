81fd824647d459689af49ba588d7f7a4d2b73b09
Charles Walton <pythontutorials36@gmail.com>
2025-01-21 20:06:45 +0100

    Registering Model to Admin for easy management

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Notebook, Note, NoteCategory
 
-# Register your models here.
+@admin.register(Notebook)
+class NotebookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'owner')
+    search_fields = ('name', 'description')
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'notebook', 'category', 'is_archived', 'is_pinned')
+    list_filter = ('is_archived', 'is_pinned', 'category')
+    search_fields = ('title', 'content')
+
+@admin.register(NoteCategory)
+class NoteCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
