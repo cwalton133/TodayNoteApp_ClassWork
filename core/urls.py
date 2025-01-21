b2d479e27271709981083fb5b987d84f7c9ada8d
Charles Walton <pythontutorials36@gmail.com>
2025-01-21 19:55:27 +0100

    Note Templates 2 Modified

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,22 @@
+# core/urls.py
+
 from django.urls import path
 from .views import (
-    NotebookListView, NotebookDetailView, NotebookCreateView, NotebookUpdateView, NotebookDeleteView,
-    NoteListView, NoteDetailView, NoteCreateView, NoteUpdateView, NoteDeleteView
+    NotebookListView, NotebookDetailView, NotebookCreateView,
+    NotebookUpdateView, NotebookDeleteView,
+    NoteListView, NoteDetailView, NoteCreateView,
+    NoteUpdateView, NoteDeleteView,
 )
 
 urlpatterns = [
-    path('core/', NotebookListView.as_view(), name='notebook-list'),
-    path('core/<int:pk>/', NotebookDetailView.as_view(), name='notebook-detail'),
-    path('core/create/', NotebookCreateView.as_view(), name='notebook-create'),
-    path('core/<int:pk>/update/', NotebookUpdateView.as_view(), name='notebook-update'),
-    path('core/<int:pk>/delete/', NotebookDeleteView.as_view(), name='notebook-delete'),
-
-    path('core/', NoteListView.as_view(), name='note-list'),
-    path('core/<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
-    path('core/create/', NoteCreateView.as_view(), name='note-create'),
-    path('core/<int:pk>/update/', NoteUpdateView.as_view(), name='note-update'),
-    path('core/<int:pk>/delete/', NoteDeleteView.as_view(), name='note-delete'),
+    path('notebooks/', NotebookListView.as_view(), name='notebook-list'),
+    path('notebooks/<int:pk>/', NotebookDetailView.as_view(), name='notebook-detail'),
+    path('notebooks/create/', NotebookCreateView.as_view(), name='notebook-create'),
+    path('notebooks/update/<int:pk>/', NotebookUpdateView.as_view(), name='notebook-update'),
+    path('notebooks/delete/<int:pk>/', NotebookDeleteView.as_view(), name='notebook-delete'),
+    path('notes/', NoteListView.as_view(), name='note-list'),
+    path('notes/<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
+    path('notes/create/', NoteCreateView.as_view(), name='note-create'),
+    path('notes/update/<int:pk>/', NoteUpdateView.as_view(), name='note-update'),
+    path('notes/delete/<int:pk>/', NoteDeleteView.as_view(), name='note-delete'),
 ]
